41d7dafad5fd876052a2684999f3ccc6e06e6849
Peter-Mateo <96933893+Peter-Mateo@users.noreply.github.com>
2022-10-19 01:11:24 -0400

    File Changes

---
+++ Practice/Practice/src/Practice/Test.java
@@ -0,0 +1,13 @@
+package Practice;
+
+public class Test {
+
+	public static void main(String[] args) {
+		testSubjects percy = new testSubjects();
+		
+		
+		// Tests 
+		String test1 = percy.setID(0)? "FAIL":"PASS";
+	}
+
+}
---
+++ Practice/omikuji-1/src/test/java/com/peterm/omkujiform/Omikuji1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peterm.omkujiform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Omikuji1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
